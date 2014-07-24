e97b24ec218148102c18d7d869d86949b312f684
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2014-07-24 18:15:22 +0100

    Limit codec offload to devices with QCOM_AV_ENHANCEMENTS enabled
    Don't even try to build this, nothing will invoke it and it may require
    non-existing definitions for compression on the device
    
    Change-Id: I1bf8055259e1dc937048269132a7e717d5fc6a01

--- hal/audio_hw.c
+++ hal/audio_hw.c
@@ -229,6 +229,7 @@ static int get_snd_codec_id(audio_format_t format)
 {
     int id = 0;
 
+#ifdef ENABLE_AV_ENHANCEMENTS
     switch (format) {
     case AUDIO_FORMAT_MP3:
         id = SND_AUDIOCODEC_MP3;
@@ -261,6 +262,7 @@ static int get_snd_codec_id(audio_format_t format)
     default:
         ALOGE("%s: Unsupported audio format :%x", __func__, format);
     }
+#endif // ENABLE_AV_ENHANCEMENTS
 
     return id;
 }
